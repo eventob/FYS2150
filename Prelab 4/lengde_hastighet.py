6069e8c67a1e6a811b9e18621c6f3327c1f97d18
_Even Tobias_ <eventob@hotmail.com>
2022-02-21 15:41:17 +0100

    Files for prelab om akselerasjon

--- Prelab 4/lengde_hastighet.py
+++ Prelab 4/lengde_hastighet.py
@@ -5,5 +5,27 @@ from scipy.stats import probplot
 from scipy.stats import linregress
 
 
+# oppgave 3
+def sound_speed(temp, fm_f=0.999):
+    c = 331.1 + (0.606 * temp)
+    return c * (1 - (1 / fm_f))
+
+
+print(sound_speed(25))
+
+# oppgave 4
+dv_v = 1 / 10   # hastighetsoppløsning
+s_freq = 10     # [kHz]
+c = 343.12      # lydhastighet [m/s]
+
+
+def hast_tid_maaling(freq, dv, N):
+    d_f = freq * ((c / (c - dv)) - 1) * 1000
+    t = N * (1 / d_f)
+    dist = 1 * t
+    return d_f, t, dist
+
+
+print(hast_tid_maaling(s_freq, dv_v, 5))
 
 
--- Prelab 4/linjetilpasning_LHA.py
+++ Prelab 4/linjetilpasning_LHA.py
@@ -1,32 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
+
 a = 3.5
 b = 5.0
-x = np.arange(0,10,0.1)
-y = a + b*x + (np.random.randn(len(x)))
+x = np.arange(0, 10, 0.1)
+y = a + b * x + (np.random.randn(len(x)))
 plt.style.use("seaborn")
-plt.plot(x,y,"*",label="data")
+plt.plot(x, y, "*", label="data")
 
-fit = stats.linregress(x,y)
+fit = stats.linregress(x, y)
 
 m = fit.slope
 c = fit.intercept
 
-plt.plot(x,m*x+c,label="linfit:{:.3f} x + {:.3f}".format(m,c))
+plt.plot(x, m * x + c, label="linfit:{:.3f} x + {:.3f}".format(m, c))
 plt.xlabel("x")
 plt.ylabel("y")
 plt.legend()
 plt.show()
 
-
-#standardfeil i estimatene
+# standardfeil i estimatene
 
 dm = fit.stderr
 dc = fit.intercept_stderr
 
-#R2 sier not om goodness of fit. 0 er veldig dårlig, 1 er veldig bra.
-R2 = fit.rvalue**2
+# R2 sier not om goodness of fit. 0 er veldig dårlig, 1 er veldig bra.
+R2 = fit.rvalue ** 2
 
 print("SE(m) = {:.3f}".format(dm))
 print("SE(c) = {:.3f}".format(dc))
