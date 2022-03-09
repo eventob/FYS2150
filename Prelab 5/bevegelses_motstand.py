81d9f35d4affe0b76eef22f3fe61a7659180f298
_Even Tobias_ <eventob@hotmail.com>
2022-03-09 11:53:12 +0100

    Jobber med prelab 5 (bev.motstand)

--- Prelab 5/bevegelses_motstand.py
+++ Prelab 5/bevegelses_motstand.py
@@ -3,22 +3,27 @@ import matplotlib.pyplot as plt
 from scipy.stats import linregress
 
 
-def terminal_rayleigh(f_d, f_g, rho, r, vel):
+def terminal_rayleigh(d):
     """
     (Turbulent fall)
     Funksjonen regner ut terminalhastigheten til objektet ved Rayleigh strømning.
     """
-    c_r = f_d / (rho * r ** 2 * vel ** 2)
-    return np.sqrt(f_g / (c_r * rho * r ** 2))
+    # c_r = f_d / (rho * r ** 2 * vel ** 2)
+    return d ** (1/2)
 
 
-def terminal_stokes(f_d, mass, grav, mu, r, vel):
+def terminal_stokes(rho_k, rho_m, nu, d):
     """
     (Laminært fall)
     Funksjonen regner ut terminalhastigheten til objektet ved Stokes strømning.
     """
-    c_s = f_d / (mu * r * vel)
-    return c_s, (mass * grav) / (c_s * mu * r)
+    g = 9.81
+    gamma = 1/18
+    C_s = 6 * np.pi     # stokes-koeffisienten av en kule
+
+    # feil nu?
+    g_hat = ((rho_k / rho_m) - 1) * gamma * g
+    return C_s, (g_hat / nu) * (d ** 2)
 
 
 def terminal_plot(arr_data, arr_label):
@@ -51,12 +56,33 @@ def terminal_video(frames, fps=60, length=25.5):
     print("Terminalhastighet fra video: %.5g [mm/s]" % terminal)
     return terminal
 
-rho_staal = 7905 - 886      # forskjellen i massetetthet mellom kulene og mediumet (stål og olje)
+
+rho_kuler = [21.1, 1042, 7905]      # tetthet for (isopor, plast, stål) [kg / m^3]
+rho_medium = [1.225, 889, 998]      # tetthet mediumer (luft, olje, vann) [kg / m^3]
+nu_medium = [15.2 * 1e-6, 214 * 1e-6, 1.0 * 1e-6]        # viskositet mediumer (luft, olje, vann) [10^-6 m^2 / s]
+mu_medium = [1.862 * 1e-5, 0.190, 0.998 * 1e-3]     # dynamisk viskositet (luft, olje, vann) [kg / ms]
+
+d_stal = 7905 - 886      # forskjellen i massetetthet mellom kulene og mediumet (stål og olje)
 mu_olje = 0.190
 
 data = np.loadtxt("terminal_hastighet_rdata.dat")
 navn = [0, "Rayleigh", "Annen", "Stoke"]
 
-linear_regression(data[0], data[1], "Stokes'")
-terminal_video(6.5, 30)
-terminal_plot(data, navn)
+diameter = np.linspace(0.005, 0.1, 10000)
+print(diameter)
+c_s, veloc = terminal_stokes(rho_kuler[2], rho_medium[1], nu_medium[1], diameter)
+# veloc_r = terminal_rayleigh(diameter)
+print(veloc[0])
+
+plt.plot(diameter, veloc * 100)
+plt.plot(diameter[0], veloc[0], 'ro')
+# plt.plot(diameter, veloc)
+plt.xscale('log'), plt.yscale('log')
+plt.xlabel('Diameter [mm]'), plt.ylabel('Terminal hastighet $v_T (d)$ [mm/s]')
+plt.ylim(8, 1e4)
+plt.grid(which='both')
+plt.show()
+
+# linear_regression(data[0], data[1], "Stokes'")
+# terminal_video(6.5, 30)
+# terminal_plot(data, navn)
