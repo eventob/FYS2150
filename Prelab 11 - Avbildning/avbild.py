3441828e42a0fbe335db0fbaa5c232feacc6199f
Even Tobias <eventob@hotmail.com>
2022-05-11 12:00:49 +0200

    Prelab 11 done

--- Prelab 11 - Avbildning/avbild.py
+++ Prelab 11 - Avbildning/avbild.py
@@ -2,5 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+print((11.5 ** 2 + 0.1 ** 2) / (2 * 0.1))
+
+R = 280 / 1e3
+n = 1.520
+print(abs((n - 1) * ((-1/R) - (1/R)) * 1e3))
 
 
+def r(x, d):
+    return (x ** 2 + d ** 2) / (2 * d)
